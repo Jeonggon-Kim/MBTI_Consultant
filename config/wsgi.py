422e6c9d785191acfcfee41b161ee5a46b4b2eaf
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-05-26 14:51:31 +0900

    wsgi 설정

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
+import site 
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))) #추가된거 각 위치경로마다 다름
 
 application = get_wsgi_application()
