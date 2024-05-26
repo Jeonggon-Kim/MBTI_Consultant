294fe25d2ce78746fdfe07ba2d4fcae5a2997d9e
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-05-26 15:21:04 +0900

    debug, allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o)pa%k+*be(&_29&ek7hfei%v7)ds-xk(mglol!f!!#_(kwokx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
