c2a97cee8daf22a99daeda68adc74c59fbdd8cd4
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-05-26 15:32:16 +0900

    dd

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))) #추가된거 각 위치경로마다 다름
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) #추가된거 각 위치경로마다 다름
 
 application = get_wsgi_application()
